308f7aff9afcb53517eeab921d0ec3c3dd3c60fd
Diusrex <diusrex@gmail.com>
2017-09-29 21:25:43 -0600

    Added UCB.

--- reinforcement_learning/multi_armed_bandits/examples/normal_distribution_bandits.py
+++ reinforcement_learning/multi_armed_bandits/examples/normal_distribution_bandits.py
@@ -6,6 +6,7 @@ sys.path.append(dir_path + "/../../..")
 
 from reinforcement_learning.explore_exploit.estimator import AverageEstimator
 from reinforcement_learning.explore_exploit.greedy_variants import GreedyExploreExploit, EpsilonGreedyExploreExploit, GreedyOptimisticInitExploreExploit
+from reinforcement_learning.explore_exploit.ucb import UCB
 from reinforcement_learning.multi_armed_bandits.util import run_multiple_times
 from reinforcement_learning.multi_armed_bandits.bandits import KNormalDistributionBandits
 from reinforcement_learning.multi_armed_bandits.graphing import GraphCumulativeRegret, GraphMeanRegret, GraphMeanPercentChoseBestAtStep
@@ -16,11 +17,10 @@ def main(num_runs=20, num_steps=500):
     actions = bandits.actions()
     
     approaches = [GreedyExploreExploit(actions, AverageEstimator()),
-                 EpsilonGreedyExploreExploit(0.1, actions, AverageEstimator()),
-                 EpsilonGreedyExploreExploit(0.5, actions, AverageEstimator()),
-                 GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 5, 2),
-                 GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 5, 10),
-                 GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 5, 50)]
+                  EpsilonGreedyExploreExploit(0.1, actions, AverageEstimator()),
+                  GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 5, 2),
+                  GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 5, 10),
+                  UCB(actions, AverageEstimator()),]
     
     algorithm_total_rewards, algorithm_regret_by_step =\
         run_multiple_times(num_runs, num_steps, bandits, approaches)
--- reinforcement_learning/multi_armed_bandits/examples/simple_percentage_bandits.py
+++ reinforcement_learning/multi_armed_bandits/examples/simple_percentage_bandits.py
@@ -6,6 +6,7 @@ sys.path.append(dir_path + "/../../..")
 
 from reinforcement_learning.explore_exploit.estimator import AverageEstimator
 from reinforcement_learning.explore_exploit.greedy_variants import GreedyExploreExploit, EpsilonGreedyExploreExploit, GreedyOptimisticInitExploreExploit
+from reinforcement_learning.explore_exploit.ucb import UCB
 from reinforcement_learning.multi_armed_bandits.util import run_multiple_times
 from reinforcement_learning.multi_armed_bandits.bandits import MultiArmedBandit, PercentageBandit
 from reinforcement_learning.multi_armed_bandits.graphing import GraphCumulativeRegret, GraphMeanRegret, GraphMeanPercentChoseBestAtStep
@@ -19,7 +20,8 @@ def main(num_runs=20, num_steps=500):
                  EpsilonGreedyExploreExploit(0.1, actions, AverageEstimator()),
                  GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 1, 2),
                  GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 1, 10),
-                 GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 1, 50)]
+                 GreedyOptimisticInitExploreExploit(actions, AverageEstimator(), 1, 50),
+                 UCB(actions, AverageEstimator()),]
     
     algorithm_total_rewards, algorithm_regret_by_step =\
         run_multiple_times(num_runs, num_steps, bandits, approaches)
